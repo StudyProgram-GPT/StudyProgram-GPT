2fda1261bfbc8d435dfe04b36930df34a1670dbf
yoshito0121 <105184147+k20111kk@users.noreply.github.com>
2023-05-09 16:27:33 +0900

    Add files via upload

---
+++ front/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/submit_code", methods=["POST"])
+def submit_code():
+    code = request.form["code"]
+    url = "http://localhost:8080/submit_code"  # ここにバックエンドのURLを入れてください
+    response = requests.post(url, data={"code": code})
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
