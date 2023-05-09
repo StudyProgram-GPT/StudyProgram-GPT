0e923004fe284392d21748f2c6ee611fa500901f
Macha_Nai <95839522+Macha-Nai@users.noreply.github.com>
2023-05-09 16:32:19 +0900

    goのapiのurlを追加

--- front/app.py
+++ front/app.py
@@ -4,16 +4,19 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/submit_code", methods=["POST"])
 def submit_code():
     code = request.form["code"]
-    url = "http://localhost:8080/submit_code"  # ここにバックエンドのURLを入れてください
+    url = "http://localhost:8080//content"  # ここにバックエンドのURLを入れてください
     response = requests.post(url, data={"code": code})
     return jsonify(response.json())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
